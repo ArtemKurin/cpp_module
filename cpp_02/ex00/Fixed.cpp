1cb5c9ba175c91b807b68e2b84992607aa92c4c0
PrideEscanor <artemikurin@icloud.com>
2022-02-20 23:07:19 +0300

    cpp02 ex00 operator=

--- cpp_02/ex00/Fixed.cpp
+++ cpp_02/ex00/Fixed.cpp
@@ -17,6 +17,13 @@ Fixed::~Fixed(void)
 	std::cout << "Destructor called" << std::endl;
 }
 
+Fixed	&operator= (const Fixed &other)
+{
+	std::cout << "Assignation operator called" << std::endl;
+	this->num = other->other.getRawBits();
+	return (*this);
+}
+
 void	Fixed::getRawBits(void)
 {
 	std::cout << "getRawBits member function called" << std::endl;
